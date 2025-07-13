88687d88f1628c5afa761740fbab3b49e1573d78
Huste6 <quannghihai1@gmail.com>
2025-07-13 10:48:31 +0700

    upload image + upload audio in client + testing audio wave

--- spotify_clone_fixed/lib/cor/utils.dart
+++ spotify_clone_fixed/lib/cor/utils.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String content) {
@@ -6,4 +9,30 @@ void showSnackBar(BuildContext context, String content) {
     ..showSnackBar(
       SnackBar(content: Text(content)),
     );
+}
+
+Future<File?> pickImage() async {
+  try {
+    final filePickerRes =
+        await FilePicker.platform.pickFiles(type: FileType.image);
+    if (filePickerRes != null && filePickerRes.files.isNotEmpty) {
+      return File(filePickerRes.files.first.path!);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
+
+Future<File?> pickAudio() async {
+  try {
+    final filePickerRes =
+        await FilePicker.platform.pickFiles(type: FileType.audio);
+    if (filePickerRes != null && filePickerRes.files.isNotEmpty) {
+      return File(filePickerRes.files.first.path!);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
 }
--- spotify_clone_fixed/lib/features/home/view/pages/upload_song_page.dart
+++ spotify_clone_fixed/lib/features/home/view/pages/upload_song_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:client/cor/theme/app_pallete.dart';
+import 'package:client/cor/utils.dart';
 import 'package:client/cor/widgets/custom_field.dart';
+import 'package:client/features/home/view/widgets/audio_wave.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +20,25 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
   final songNameController = TextEditingController();
   final artistController = TextEditingController();
   Color selectedColor = Pallete.cardColor;
+  File? selectedImage;
+  File? selectedAudio;
+  void selectAudio() async {
+    final pickedAudio = await pickAudio();
+    if (pickedAudio != null) {
+      setState(() {
+        selectedAudio = pickedAudio;
+      });
+    }
+  }
+
+  void selectImage() async {
+    final pickedImage = await pickImage();
+    if (pickedImage != null) {
+      setState(() {
+        selectedImage = pickedImage;
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -37,42 +60,61 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
           padding: const EdgeInsets.symmetric(horizontal: 10.0),
           child: Column(
             children: [
-              DottedBorder(
-                color: Pallete.borderColor,
-                radius: const Radius.circular(10),
-                dashPattern: const [5, 5],
-                borderType: BorderType.RRect,
-                strokeCap: StrokeCap.round,
-                child: const SizedBox(
-                  height: 180,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        'Select the thumbnail for your song',
-                        style: TextStyle(fontSize: 15),
+              GestureDetector(
+                onTap: selectImage,
+                child: selectedImage != null
+                    ? SizedBox(
+                        height: 150,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            selectedImage!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       )
-                    ],
-                  ),
-                ),
+                    : DottedBorder(
+                        color: Pallete.borderColor,
+                        radius: const Radius.circular(10),
+                        dashPattern: const [5, 5],
+                        borderType: BorderType.RRect,
+                        strokeCap: StrokeCap.round,
+                        child: const SizedBox(
+                          height: 180,
+                          width: double.infinity,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                'Select the thumbnail for your song',
+                                style: TextStyle(fontSize: 15),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
               ),
               const SizedBox(
                 height: 40,
               ),
-              CustomField(
-                hintText: 'Pick Song',
-                controller: null,
-                readOnly: true,
-                onTap: () {},
-              ),
+              selectedAudio != null
+                  ? AudioWave(
+                      path: selectedAudio!.path,
+                    )
+                  : CustomField(
+                      hintText: 'Pick Song',
+                      controller: null,
+                      readOnly: true,
+                      onTap: selectAudio,
+                    ),
               const SizedBox(
                 height: 20,
               ),
