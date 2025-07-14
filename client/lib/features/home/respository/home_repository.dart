da9e9659bfc160acb221bbb7ce19748aaa5a5e8c
Huste6 <quannghihai1@gmail.com>
2025-07-14 14:45:48 +0700

    upload song in client

--- client/lib/features/home/respository/home_repository.dart
+++ client/lib/features/home/respository/home_repository.dart
@@ -1,33 +1,63 @@
 import 'dart:io';
-
 import 'package:client/cor/constants/server_constant.dart';
+import 'package:client/cor/failure/failure.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class HomeRespository {
-  Future<void> uploadSong(File selectedImage, File selectedAudio) async {
-    final request = http.MultipartRequest(
-      'POST',
-      Uri.parse('${ServerConstant.serverUrl}/song/upload'),
-    );
-    request
-      ..files.addAll(
-        [
-          await http.MultipartFile.fromPath(
-              'song', selectedAudio.path),
-          await http.MultipartFile.fromPath('thumbnail',
-              selectedImage.path)
-        ],
-      )
-      ..fields.addAll(
-        {'artist': 'Quan', 'song_name': 'thien menh', 'hex_code': 'FFFFFF'},
-      )
-      ..headers.addAll(
-        {
-          'x-auth-token':
-              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTh9.zaEGDL4jdN-GnPFuZAQzGLxVcRUOqxDEdQQaQLzJ_1A'
-        },
+part 'home_repository.g.dart';
+
+@riverpod
+HomeRepository homeRepository(HomeRepositoryRef ref) {
+  return HomeRepository();
+}
+
+class HomeRepository { // Corrected typo
+  Future<Either<AppFailure, String>> uploadSong({
+    required File selectedAudio,
+    required File selectedThumbnail,
+    required String songname,
+    required String artist,
+    required String hex_code,
+    required String token,
+  }) async {
+    try {
+      final request = http.MultipartRequest(
+        'POST',
+        Uri.parse('${ServerConstant.serverUrl}/song/upload'),
       );
-    final res = await request.send();
-    print(res);
+      request
+        ..files.addAll(
+          [
+            await http.MultipartFile.fromPath(
+              'song',
+              selectedAudio.path,
+
+            ),
+            await http.MultipartFile.fromPath(
+              'thumbnail',
+              selectedThumbnail.path,
+
+            )
+          ],
+        )
+        ..fields.addAll(
+          {'artist': artist, 'song_name': songname, 'hex_code': hex_code},
+        )
+        ..headers.addAll(
+          {'x-auth-token': token},
+        );
+
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        return Right(response.body);
+      } else {
+        return Left(AppFailure('Server error: ${response.body}'));
+      }
+    } catch (e) {
+      return Left(AppFailure(e.toString()));
+    }
   }
 }
---
+++ client/lib/features/home/viewmodel/home_viewmodel.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+import 'dart:ui';
+import 'package:client/cor/providers/current_user_notifier.dart';
+import 'package:client/cor/utils.dart';
+import 'package:client/features/home/respository/home_repository.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'home_viewmodel.g.dart';
+
+@riverpod
+class HomeViewModel extends _$HomeViewModel {
+  late HomeRepository _homeRespository;
+  @override
+  AsyncValue? build() {
+    _homeRespository = ref.watch(homeRepositoryProvider);
+    return null;
+  }
+
+  Future<void> uploadSong({
+    required File selectedAudio,
+    required File selectedThumbnail,
+    required String songname,
+    required String artist,
+    required Color selectedColor,
+  }) async {
+    state = const AsyncValue.loading();
+    final res = await _homeRespository.uploadSong(
+      selectedAudio: selectedAudio,
+      selectedThumbnail: selectedThumbnail,
+      songname: songname,
+      artist: artist,
+      hex_code: rgbToHex(selectedColor),
+      token: ref.read(currentUserNotifierProvider)!.token,
+    );
+    final val = switch (res) {
+      Left(value: final l) => state = AsyncValue.error(l.message, StackTrace.current),
+      Right(value: final r) => state = AsyncValue.data(r),
+    };
+    print(val);
+  }
+}
