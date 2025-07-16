7cb53e35e4af7ef70118ac9f39ce82917577e01e
Huste6 <quannghihai1@gmail.com>
2025-07-16 09:29:16 +0700

    Play/Pause Music, Display Song Progress

--- client/lib/cor/providers/current_song_notifier.dart
+++ client/lib/cor/providers/current_song_notifier.dart
@@ -6,6 +6,7 @@ part 'current_song_notifier.g.dart';
 @riverpod
 class CurrentSongNotifier extends _$CurrentSongNotifier{
   AudioPlayer? audioPlayer;
+  bool isPlaying = false;
 
   @override
   SongModel? build(){
@@ -17,7 +18,29 @@ class CurrentSongNotifier extends _$CurrentSongNotifier{
     final audioSource = AudioSource.uri(Uri.parse(song.song_url));
     await audioPlayer!.setAudioSource(audioSource);
 
+    audioPlayer!.playerStateStream.listen((state){
+      if(state.processingState == ProcessingState.completed){
+        audioPlayer!.seek(Duration.zero);
+        audioPlayer!.pause();
+        isPlaying = false;
+        this.state = this.state?.copyWith(hexCode: this.state?.hexCode);
+      }
+    });
+
     audioPlayer!.play();
+    isPlaying = true;
     state = song;
+  }
+
+  void playPause(){
+    if(isPlaying){
+      audioPlayer?.pause();
+    }else{
+      audioPlayer?.play();
+    }
+    isPlaying = !isPlaying;
+    // Câu này thoạt nhìn thì sẽ tưởng vô nghĩa nhưng nó gọi cpwith để tạo 1 object y chang cái cũ, chỉ để ép riverpod nhận ra là "state đã thay đổi" dù giá trị không đổi
+    // Tác dụng ép các widget rebuild từ đó có thể đổi được icon pause/play
+    state = state?.copyWith(hexCode: state?.hexCode);
   }
 }
